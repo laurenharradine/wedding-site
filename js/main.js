0acfbbf1a5310ade11a385e7bf99f6ccb6e6daa0
Lauren Harradine <lhharradine@gmail.com>
2022-10-22 13:25:20 +1100

    Update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 let slideIndex = 0;
 showSlides();
 
+let pfIndex = 0;
+showSlides2(pfIndex);
+
 function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
@@ -13,12 +16,28 @@ function showSlides() {
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
 
+function showSlides2(n) {
+  let i;
+  let slides = document.getElementsByClassName("pf");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides2(slideIndex += n);
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides2(slideIndex = n);
 }
